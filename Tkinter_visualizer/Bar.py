1ad438172333377589d4e8f57d26d9f77ffe42ee
kedar <kekniskedar@gmail.com>
2024-04-21 10:20:53 +0530

    04/21/24 10:20:53 — main: changing bar widget from progressbar to cnavas.rectangel Signed-off-by: kedar <kekniskedar@gmail.com>

--- Tkinter_visualizer/Bar.py
+++ Tkinter_visualizer/Bar.py
@@ -3,13 +3,17 @@ from ttkbootstrap import ttk
 import tkinter as tk
 
 
-class Bar(tk.Canvas.create_rectangle()):
-    def __init__(self, **kwargs: dict) -> None:
-        win_height = kwargs['win_height']
-        height = randint(1, 100)
-        height_pct = height / 100
-        self.bar_height = round(height_pct * win_height) - 40
-        self.bar_color = "%06x" % randint(0, 0xFFFFFF)
+class Bar:
+    def __init__(self, master: tk.Tk, bar_height: float) -> None:
+        B_STYLE = choice(['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'dark'])
+        # height = randint(1, 100)
+        # height_pct = height / 100
+        self.bar_height = bar_height
+        self.pb = ttk.Progressbar(
+            master=master,
+            orient='vertical',
+            length=self.bar_height,
+            value=100,
+            bootstyle=B_STYLE,
 
-    def create_bar(self, **kwargs):
-        return self.create_rectangle((5, self.bar_height, 25, 5), fill=self.bar_color, width=2)
+        )
--- Tkinter_visualizer/main.py
+++ Tkinter_visualizer/main.py
@@ -1,19 +1,97 @@
-from tkinter import *
-import random
+import time
+import tkinter as tk
+from random import shuffle
+from tkinter import ttk
+from Bar import Bar
 
-GAME_WIDTH = 900
-GAME_HEIGHT = 500
-BACKGROUND_COLOR = "#000000"
 
-window = Tk()
-window.title('Sorting Visualizer')
-window.resizable(False, True)
+def bubbleSort(array: list) -> None:
+    is_sorted = False
+    counter = 0
+    while not is_sorted:
+        is_sorted = True
+        for idx in range(len(array) - 1 - counter):
+            if array[idx].bar_height > array[idx + 1].bar_height:
+                bubble_swap(idx, array)
+                is_sorted = False
+            # updateBarPosition()
+        counter += 1
 
-BAR_COUNT = tuple(range(50))
 
-canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
-canvas.pack()
+def insertionSort(array: list) -> None:
+    for idx in range(1, len(array)):
+        curr_idx = idx
+        while curr_idx > 0 and array[curr_idx].bar_height < array[curr_idx - 1].bar_height:
+            insertion_swap(curr_idx, array)
+            curr_idx -= 1
+        # updateBarPosition()
 
-window.update()
 
-window.mainloop()
+def selectionSort(array: list) -> list:
+    currentIdx = 0
+    while currentIdx < len(array) - 1:
+        smallestIdx = currentIdx
+        for i in range(currentIdx + 1, len(array)):
+            if array[smallestIdx].bar_height > array[i].bar_height:
+                smallestIdx = i
+            # updateBarPosition()
+        selection_swap(currentIdx, smallestIdx, array)
+        currentIdx += 1
+    return array
+
+
+def selection_swap(i: int, j: int, array: list) -> None:
+    array[i], array[j] = array[j], array[i]
+    updateBarPosition()
+
+
+def insertion_swap(idx: int, array: list) -> None:
+    array[idx], array[idx - 1] = array[idx - 1], array[idx]
+    updateBarPosition()
+
+
+def bubble_swap(idx: int, array: list) -> None:
+    array[idx], array[idx + 1] = array[idx + 1], array[idx]
+    updateBarPosition()
+
+
+def updateBarPosition():
+    global bars
+    for _idx, _bar in enumerate(bars):
+        _bar.pb.grid(row=0, column=_idx, sticky='ews', padx=1, pady=(0, 35))
+    window.update()
+
+
+def sortBars():
+    global bars
+    bubbleSort(bars)
+    # insertionSort(bars)
+    # selectionSort(bars)
+
+
+if __name__ == '__main__':
+    # window
+    window = tk.Tk()
+    window.title('Sorting Algorithms')
+    wondow_width = 900
+    window_height = 500
+    window.geometry(f'{str(wondow_width)}x{str(window_height)}')
+    # window.resizable(False, True)
+
+    # grid layout for bars printing
+    a = tuple(range(150))
+    window.columnconfigure(a, weight=1, uniform='a')
+    window.rowconfigure(0, weight=1)
+
+    # list of bars
+    height_mul = window_height / len(a)
+    bars = [Bar(window, i * height_mul) for i in range(len(a))]
+    shuffle(bars)
+
+    updateBarPosition()
+
+    sort_button = ttk.Button(master=window, text='Sort', command=sortBars)
+    sort_button.place(relx=0.48, rely=0.93)
+
+    # run
+    window.mainloop()
